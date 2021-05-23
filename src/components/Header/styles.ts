7d9f49d32f97f67edd8b999e5c159f8968eeda9a
NicholasNeto <nicholasneto@outlook.com>
2021-05-23 20:03:34 -0300

    fix same rules in styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-    input {
-       // border: none;
+    label {
+        color: red;
     }
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 import 'react-toastify/dist/ReactToastify.css';
 
-export default createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
@@ -9,7 +9,15 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
-    .container {
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75% //15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5% // 14px
+        }
+
         max-width: 1020px;
         margin: 0 auto;
         padding: 0 20px 50px;
