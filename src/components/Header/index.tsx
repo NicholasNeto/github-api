3feb4ee584b02b144fae7e24abd73a62a739afd9
NicholasNeto <nicholasneto@outlook.com>
2021-05-23 20:03:02 -0300

    add elements in page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
 
 import { Container } from "./styles";
 
-export function Header() {
+
+interface HeaderProps {
+    user : { 
+        name: string;
+        email: string;
+    }
+}
+
+export function Header({ user }: HeaderProps ) {
     return (
         <Container>
             <label>
                 <input type="text" placeholder="Digite username" />
             </label>
+
+            <div>
+                <div>
+                    <img src="https://avatars.githubusercontent.com/u/19715382?v=4" alt="" />
+                </div>
+                <div>
+                    <h1>{user.name}</h1>
+                    <div>Email: {user.email}</div>
+                </div>
+            </div>
         </Container>
     )
 }
