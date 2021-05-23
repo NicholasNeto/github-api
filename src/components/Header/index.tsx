ec5504f0425afaee7be38b0dcd442f3a9f027bf6
NicholasNeto <nicholasneto@outlook.com>
2021-05-23 19:40:04 -0300

    created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+
+import { Container } from "./styles";
+
+export function Header() {
+    return (
+        <Container>
+            <label>
+                <input type="text" placeholder="Digite username" />
+            </label>
+        </Container>
+    )
+}
