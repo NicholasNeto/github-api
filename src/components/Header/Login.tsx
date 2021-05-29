ff410f1fd3c34a55b7bb3d62c8167d23e9be6d1a
NicholasNeto <nicholasneto@outlook.com>
2021-05-29 17:28:12 -0300

    created component for login

---
+++ src/components/Header/Login.tsx
@@ -0,0 +1,15 @@
+import { Avatar, Button, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+
+interface LoginProps {
+}
+
+export function Login({ }: LoginProps) {
+    return (
+        <Flex align='center'>
+            <Avatar size='md' name='Nicholas Neto' src='https://avatars.githubusercontent.com/u/19715382?v=4' />
+            <Button colorScheme='pink' >Login</Button>
+        </Flex>
+    )
+}
