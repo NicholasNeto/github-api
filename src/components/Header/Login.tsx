6613139ff2d2064f3ab257aa48c6897d5c65b6c2
NicholasNeto <nicholasneto@outlook.com>
2021-06-12 19:05:40 -0300

    add singIn with github

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -1,13 +1,31 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
 import { Avatar, Button, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { Icon } from "@chakra-ui/react"
+
 interface LoginProps {
 }
 
 export function Login({ }: LoginProps) {
+    const [session] = useSession()
+
     return (
         <Flex align='center'>
-            <Avatar marginRight='4' name='Nicholas Neto' src='https://avatars.githubusercontent.com/u/19715382?v=4' />
-            <Button colorScheme='pink' >Login</Button>
+            {session ? (
+                <>
+                   <Avatar
+                      marginRight='4'
+                      name={session.user?.name ? session.user?.name : 'teste teste' }
+                      src={session.user?.image ? session.user?.image : 'teste teste'} 
+                    />
+                    <Button colorScheme='pink' onClick={() => signOut()}>{session.user?.name}</Button>
+                </>
+            ) : (
+                <>
+                    <Icon as={FaGithub} marginRight='4' w={8} h={8} />
+                    <Button colorScheme='pink' onClick={() => signIn('github')}>Sing in with Github</Button>
+                </>
+            )}
         </Flex>
     )
 }
