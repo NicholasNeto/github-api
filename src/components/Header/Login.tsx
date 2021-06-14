ea9a03514d65833c7fe6fe81cbae153b1aa48c28
NicholasNeto <nicholasneto@outlook.com>
2021-06-13 23:10:44 -0300

    change page login

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -1,7 +1,6 @@
 import { signIn, signOut, useSession } from 'next-auth/client'
-import { Avatar, Button, Flex } from '@chakra-ui/react'
+import { Avatar, Button, Flex, Icon, useBreakpointValue } from '@chakra-ui/react'
 import { FaGithub } from 'react-icons/fa'
-import { Icon } from "@chakra-ui/react"
 
 interface LoginProps {
 }
@@ -9,20 +8,27 @@ interface LoginProps {
 export function Login({ }: LoginProps) {
     const [session] = useSession()
 
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Flex align='center'>
             {session ? (
                 <>
-                   <Avatar
-                      marginRight='4'
-                      name={session.user?.name ? session.user?.name : 'teste teste' }
-                      src={session.user?.image ? session.user?.image : 'teste teste'} 
-                    />
+                    {isWideVersion && (
+                        <Avatar
+                            marginRight='4'
+                            name={session.user?.name ? session.user?.name : 'teste teste'}
+                            src={session.user?.image ? session.user?.image : 'teste teste'}
+                        />
+                    )}
                     <Button colorScheme='pink' onClick={() => signOut()}>{session.user?.name}</Button>
                 </>
             ) : (
                 <>
-                    <Icon as={FaGithub} marginRight='4' w={8} h={8} />
+                    {isWideVersion && <Icon as={FaGithub} marginRight='4' w={8} h={8} />}
                     <Button colorScheme='pink' onClick={() => signIn('github')}>Sing in with Github</Button>
                 </>
             )}
